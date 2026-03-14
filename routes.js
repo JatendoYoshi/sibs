6c8af55834fd35162e8567cd7a730f9c9c411732
JatendoYoshi <jasonleungyuihang@gmail.com>
2026-03-14 20:14:14 +0000

    add all routes and stops

--- routes.js
+++ routes.js
@@ -1,73 +1,376 @@
 const routes = [
 
-{number:"21", interval:12, stops:["Central","Northern Interchange","Norton Town Center"]},
+{
+number:"21",
+interval:12,
+stops:[
+"Containers Island Bus Terminus",
+"Dove Estate",
+"Sunshine Stadium",
+"Central",
+"Northern Interchange"
+]
+},
 
-{number:"25", interval:15, stops:["Southern Central","Central","Northern Interchange"]},
+{
+number:"25",
+interval:15,
+stops:[
+"Containers Island Bus Terminus",
+"Dove Estate",
+"Sunshine Stadium",
+"Long Island Ferry Pier"
+]
+},
 
-{number:"41A", interval:15, stops:["Sunshine Pier","Central","Northern Interchange","Northern Beach"]},
+{
+number:"41A",
+interval:10,
+stops:[
+"Sunshine Pier",
+"Sunshine Station",
+"Central",
+"Northern Interchange",
+"Northern Beach"
+]
+},
 
-{number:"42", interval:20, stops:["Southern Central","Sunshine Station","Central Hospital","Northern Interchange","Rocky Road"]},
+{
+number:"42",
+interval:20,
+stops:[
+"Southern Central",
+"Southern Cultural District",
+"Sunshine Station",
+"Central Hospital",
+"Northern Interchange",
+"Rocky Road"
+]
+},
 
-{number:"42A", interval:15, stops:["Southern Central","Sunshine Station","Central Hospital","Sunshine Pier"]},
+{
+number:"42A",
+interval:15,
+stops:[
+"Sunshine Pier",
+"Southern Cultural District",
+"Sunshine Station",
+"Central Hospital",
+"Rocky Road"
+]
+},
 
-{number:"46", interval:12, stops:["Central","Eastmallow","Norton Town Center"]},
+{
+number:"46",
+interval:12,
+stops:[
+"Central",
+"Eastmallow",
+"Norton Town Center"
+]
+},
 
-{number:"47", interval:12, stops:["Sunshine University","Eastmallow","Central Hospital","Sunshine Station","Central"]},
+{
+number:"47",
+interval:12,
+stops:[
+"Sunshine University",
+"Eastmallow",
+"Central Hospital",
+"Sunshine Station",
+"Central"
+]
+},
 
-{number:"49A", interval:20, stops:["Northern Interchange","Mountain Estate","Northern Hills"]},
+{
+number:"49A",
+interval:20,
+stops:[
+"Sunshine Pier",
+"Sunshine Station",
+"Northern Interchange",
+"Northern Beach"
+]
+},
 
-{number:"73", interval:15, stops:["Southern Cultural District","Central","Northern Interchange"]},
+{
+number:"73",
+interval:15,
+stops:[
+"Southern Cultural District",
+"Southern",
+"Central",
+"Northern Interchange"
+]
+},
 
-{number:"74A", interval:10, stops:["Central","Axis Road","Northern Interchange"]},
+{
+number:"74A",
+interval:10,
+stops:[
+"Central",
+"Axis Road",
+"Northern Interchange"
+]
+},
 
-{number:"75", interval:10, stops:["Central","Rainbow Estate","Norton Town Center"]},
+{
+number:"75",
+interval:10,
+stops:[
+"Central",
+"Rainbow Estate",
+"Norton Town Center"
+]
+},
 
-{number:"75P", interval:20, stops:["Central","Rainbow Estate","Axis Road"]},
+{
+number:"75P",
+interval:20,
+stops:[
+"Central",
+"Rainbow Estate"
+]
+},
 
-{number:"76", interval:12, stops:["Southern","Central","Norton Town Center"]},
+{
+number:"76",
+interval:12,
+stops:[
+"Southern",
+"Central",
+"Norton Town Center"
+]
+},
 
-{number:"77", interval:15, stops:["Southern","Central","Northern Interchange"]},
+{
+number:"77",
+interval:15,
+stops:[
+"Southern",
+"Central",
+"Northern Interchange"
+]
+},
 
-{number:"140", interval:10, stops:["Central","North Island Estate","Norton Town Center"]},
+{
+number:"140",
+interval:10,
+stops:[
+"Long Island Ferry Pier",
+"Sunshine Stadium",
+"Central",
+"Sunshine Pier"
+]
+},
 
-{number:"140P", interval:20, stops:["Central","North Island Estate"]},
+{
+number:"140P",
+interval:20,
+stops:[
+"Central",
+"Sunshine Stadium"
+]
+},
 
-{number:"141P", interval:20, stops:["Central","Axis Road","Zone 7 Interchange"]},
+{
+number:"141P",
+interval:20,
+stops:[
+"Central",
+"Axis Road",
+"Zone 7 Interchange"
+]
+},
 
-{number:"142", interval:12, stops:["Central","Northern Interchange","Leafy University"]},
+{
+number:"142",
+interval:12,
+stops:[
+"Dove Estate",
+"Central",
+"Sunshine Pier"
+]
+},
 
-{number:"148", interval:10, stops:["Containers Island","Hospital Island","Dove Island","Northern Interchange","Sunshine University","Norton Town Center"]},
+{
+number:"148",
+interval:10,
+stops:[
+"Containers Island Bus Terminus",
+"Dove Estate",
+"Northern Interchange",
+"Sunshine University",
+"Norton Town Center"
+]
+},
 
-{number:"171", interval:15, stops:["Central","Northern Interchange","Zone 7 Interchange"]},
+{
+number:"171",
+interval:15,
+stops:[
+"Central",
+"Northern Interchange",
+"Zone 7 Interchange"
+]
+},
 
-{number:"246X", interval:20, stops:["Central","Axis Road","Outer Expressway"]},
+{
+number:"246X",
+interval:20,
+stops:[
+"Central",
+"Axis Road",
+"Zone 7 Interchange"
+]
+},
 
-{number:"370", interval:12, stops:["Central","Axis Road","Zone 7 Interchange"]},
+{
+number:"370",
+interval:12,
+stops:[
+"Central",
+"Axis Road",
+"Zone 7 Interchange"
+]
+},
 
-{number:"370A", interval:15, stops:["Central","Axis Road","Leafy Bay"]},
+{
+number:"370A",
+interval:15,
+stops:[
+"Central",
+"Axis Road",
+"Leafy Bay"
+]
+},
 
-{number:"370B", interval:15, stops:["Central","Axis Road","Rainbow Estate"]},
+{
+number:"370B",
+interval:15,
+stops:[
+"Central",
+"Axis Road",
+"Rainbow Estate"
+]
+},
 
-{number:"472", interval:15, stops:["Senpai Shopping Centre","Kamaya","Southern","Central","Norton Town Center"]},
+{
+number:"472",
+interval:15,
+stops:[
+"Senpai Shopping Centre",
+"Kamaya",
+"Zone 7 Interchange",
+"Southern",
+"Central",
+"Norton Town Center"
+]
+},
 
-{number:"473", interval:10, stops:["Central","East Factory","Maple Lane","Zone 7 Interchange"]},
+{
+number:"473",
+interval:10,
+stops:[
+"Central",
+"Maple Lane",
+"Zone 7 Interchange"
+]
+},
 
-{number:"475", interval:12, stops:["Central","Rainbow Estate","Sabow Bridge"]},
+{
+number:"475",
+interval:12,
+stops:[
+"Senpai Shopping Centre",
+"Eastmallow",
+"Rainbow Estate",
+"Norton Town Center"
+]
+},
 
-{number:"475A", interval:20, stops:["Central","Sabow Bridge","Leafy Bay"]},
+{
+number:"475A",
+interval:20,
+stops:[
+"Senpai Shopping Centre",
+"Rainbow Estate",
+"Leafy Bay"
+]
+},
 
-{number:"475P", interval:20, stops:["Central","Rainbow Estate"]},
+{
+number:"475P",
+interval:20,
+stops:[
+"Central",
+"Rainbow Estate"
+]
+},
 
-{number:"476", interval:10, stops:["Central","Leafy University","Rainbow Estate","Zone 7 Interchange"]},
+{
+number:"476",
+interval:10,
+stops:[
+"Eastmallow Praya Road",
+"Sunshine Station",
+"Rainbow Estate",
+"Zone 7 Interchange"
+]
+},
 
-{number:"476P", interval:20, stops:["Central","Leafy University"]},
+{
+number:"476P",
+interval:20,
+stops:[
+"Central",
+"Sunshine Station"
+]
+},
 
-{number:"476X", interval:20, stops:["Central","Axis Road","Zone 7 Interchange"]},
+{
+number:"476X",
+interval:20,
+stops:[
+"Central",
+"Axis Road",
+"Zone 7 Interchange"
+]
+},
 
-{number:"S1", interval:30, stops:["Long Island Ferry Pier","Timelapse Mall","Sunshine Stadium","Northern Interchange","Sun Central Street","Southern Cultural District","Central"]},
+{
+number:"S1",
+interval:30,
+stops:[
+"Long Island Ferry Pier",
+"Timelapse Mall",
+"Sunshine Stadium",
+"Northern Interchange",
+"Sun Central Street",
+"Southern Cultural District",
+"Central"
+]
+},
 
-{number:"S2", interval:30, stops:["Central","Sunshine Stadium","Northern Interchange","Long Island Ferry Pier"]},
+{
+number:"S2",
+interval:30,
+stops:[
+"Central",
+"Sunshine Stadium",
+"Northern Interchange",
+"Long Island Ferry Pier"
+]
+},
 
-{number:"U47", interval:20, stops:["Sunshine University","Central Hospital","Central"]}
+{
+number:"U47",
+interval:20,
+stops:[
+"Sunshine University",
+"Central Hospital",
+"Central"
+]
+}
 
 ];
